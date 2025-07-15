48b0b6bc0c0e214611f47356eff1085fe7f4eea5
Mateusz Okoń <mateusz.okon04@gmail.com>
2025-07-15 21:30:17 +0200

    init should be done

--- init.c
+++ init.c
@@ -1,6 +1,6 @@
 #include "philo.h"
 
-static void forks_init(t_philo *philo, t_fork *forks, int i)
+static void forks_value_init(t_philo *philo, t_fork *forks, int i)
 {
     philo->right_fork = &forks[(i+1) % philo->table->philo_nbr];
     philo->left_fork = &forks[i];
@@ -11,7 +11,7 @@ static void forks_init(t_philo *philo, t_fork *forks, int i)
     }
 }
 
-static void philo_init(t_table table)
+static void philo_value_init(t_table *table)
 {
     int i;
     t_philo *philo;
@@ -24,12 +24,28 @@ static void philo_init(t_table table)
         philo->full = false;
         philo->meals_eaten = 0;
         philo->table = table;
-        forks_init(philo, table->forks, i);
+        forks_value_init(philo, table->forks, i);
     }
 }
 void data_init(t_table *table)
 {
     table->stop_simulation = false;
+    table->start_simulation  = get_time();//idk
+
+    if(pthread_mutex_init(&table->print_mutex, NULL) != 0)
+        error_exit("Mutex init error![1]");
+
     table->philos = safe_malloc(sizeof(t_philo) * table->philo_nbr);
     table->forks = safe_malloc(sizeof(t_fork) * table->philo_nbr);
+
+    i = 0;
+    while (i < table->philo_nbr)
+    {
+        table->forks[i].id = i + 1;
+        if (pthread_mutex_init(&table->forks[i].mutex, NULL) != 0)
+            error_exit("Mutex init error![2]");
+        i++;
+    }
+    philo_value_init(table);
+
 }  
